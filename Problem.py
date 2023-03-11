6971012ea98fd24931a6b31d045c027ee81dae60
SajjadFarzane <sajjadfarzane1@gmail.com>
2023-03-11 15:04:45 +0330

    adding ucs and change cost of path

--- Problem.py
+++ Problem.py
@@ -28,7 +28,7 @@ class Problem:
                 if i == j:
                     continue
                 if not state.pipes[j].is_full() and not state.pipes[i].is_empty():
-                    s = State(copy.deepcopy(state.pipes), state, self.get_cost_from_change(state, i), (i, j))
+                    s = State(copy.deepcopy(state.pipes), state, self.get_cost_from_change(state, i, j), (i, j))
                     s.change_between_two_pipe(i, j)
                     child.append(s)
         return child
@@ -46,15 +46,15 @@ class Problem:
         out = out[:len(out) - 1] + '\n'
         return out
 
-    def get_cost_from_change(self, state: State, pipe_src_ind: int) -> int:
+    def get_cost_from_change(self, state: State, pipe_src_ind: int, pipe_dest_ind: int) -> int:
         if state.pipes[pipe_src_ind].stack[-1] == 'red':
-            return state.g_n + self.path_cost[0]
+            return state.g_n + abs(pipe_src_ind - pipe_dest_ind)
         elif state.pipes[pipe_src_ind].stack[-1] == 'blue':
-            return state.g_n + self.path_cost[1]
+            return state.g_n + abs(pipe_src_ind - pipe_dest_ind)
         elif state.pipes[pipe_src_ind].stack[-1] == 'green':
-            return state.g_n + self.path_cost[2]
+            return state.g_n + abs(pipe_src_ind - pipe_dest_ind)
         elif state.pipes[pipe_src_ind].stack[-1] == 'yellow':
-            return state.g_n + self.path_cost[3]
+            return state.g_n + abs(pipe_src_ind - pipe_dest_ind)
 
     def set_path_cost(self, cost: list):
         self.path_cost = cost
--- Search.py
+++ Search.py
@@ -1,42 +1,46 @@
 from Solution import Solution
 from Problem import Problem
 from datetime import datetime
+from PriorityQueue import PriorityQueue
 
 
 class Search:
     @staticmethod
-    def bfs(prb: Problem) -> Solution:  # this method get a first state of Problem and do bfs for find solution if no
-        # solution is find return None else return the solution
+    def bfs(prb: Problem) -> Solution:
         start_time = datetime.now()
-        queue = []
         state = prb.initState
-        queue.append(state)
+        if prb.is_goal(state):
+            return Solution(state, prb, start_time)
+        queue = [state]
+        explored = {}
+
         while len(queue) > 0:
             state = queue.pop(0)
             neighbors = prb.successor(state)
             for c in neighbors:
-                if prb.is_goal(c):
-                    return Solution(c, prb, start_time)
-                queue.append(c)
+                if c.__hash__() not in explored:
+                    if prb.is_goal(c):
+                        return Solution(c, prb, start_time)
+                    queue.append(c)
         return None
 
     @staticmethod
     def dfs(prb: Problem) -> Solution:
         start_time = datetime.now()
-        queue = []
-        explored = {}
         state = prb.initState
-        queue.append(state)
-        while len(queue) > 0:
-            state = queue.pop()
-            explored[state.__hash__()] = state
+        stack = [state]
+        explored = {}
 
+        while len(stack) > 0:
+            state = stack.pop()
+            explored[state.__hash__()] = state
             neighbors = prb.successor(state)
+
             for c in neighbors:
                 if prb.is_goal(c):
                     return Solution(c, prb, start_time)
                 if c.__hash__() not in explored:
-                    queue.append(c)
+                    stack.append(c)
         return None
 
     @staticmethod
@@ -46,26 +50,46 @@ class Search:
         explored = {}
         state = prb.initState
         queue.append(state)
+
         while len(queue) > 0:
             state = queue.pop()
             explored[state.__hash__()] = state
-
             neighbors = prb.successor(state)
+
             for c in neighbors:
                 if prb.is_goal(c):
                     return Solution(c, prb, start_time)
-                if c.__hash__() not in explored and not c.g_n > limit:
+                if c.__hash__() not in explored and c.g_n <= limit:
                     queue.append(c)
         return None
 
     @staticmethod
-    def ids(prb: Problem):
+    def ids(prb: Problem) -> Solution:
 
         limit = 0
         while True:
             res = Search.dls(prb, limit)
             limit += 1
-            if res != None:
+            if res is not None:
                 return res
 
+    @staticmethod
+    def ucs(prb: Problem):
+        start_time = datetime.now()
+        state = prb.initState
+        priority_queue = PriorityQueue()
+        priority_queue.insert(state)
+        explored = {}
 
+        while not priority_queue.isEmpty():
+            state = priority_queue.pop()
+            explored[state.__hash__()] = state
+            neighbors = prb.successor(state)
+
+            for c in neighbors:
+                if prb.is_goal(c):
+                    return Solution(c, prb, start_time)
+                if c.__hash__() not in explored:
+                    priority_queue.insert(c)
+
+        return None
